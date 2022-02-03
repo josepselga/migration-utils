e1494dc229adf3c53d422571fba988011b5498bd
Josep Selga <josep.selga@opencloudfactory.com>
2022-02-03 10:38:03 +0100

    add tests

--- generate_Ansible.php
+++ generate_Ansible.php
@@ -36,12 +36,17 @@ $analyticsArray = array();
 $sensorsArray = array();
 
 $hosts = fopen("/etc/hosts", "r");
+
 if ($hosts) {
     while (($line = fgets($hosts)) !== false) {
         // ((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])
 
-    }
+        if (preg_match('/((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])/', $lines, $hostMatch)){ 
+            echo "\nHost -> "; echo $hostMatch[1] . " --> " . $hostMatch[0] . "\n";
+        }
 
+
+    }
     fclose($hosts);
 } else {
     // error opening the file.
