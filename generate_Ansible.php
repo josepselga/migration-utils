fd684dc9a7f2517205ea682e5c6f39a28eaba26b
Josep Selga <josep.selga@opencloudfactory.com>
2022-02-03 10:50:59 +0100

    to core

--- generate_Ansible.php
+++ generate_Ansible.php
@@ -41,17 +41,39 @@ if ($hosts) {
     while (($line = fgets($hosts)) !== false) {
         // ((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])
 
-        if (preg_match('/((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])/', $lines, $hostMatch)){ 
-            echo "\nHost -> "; echo $hostMatch[1] . " --> " . $hostMatch[0] . "\n";
+        if (preg_match('/((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])/', $line, $hostMatch)){ 
+            echo "\nHost -> "; echo $hostMatch[1] . " --> " . $hostMatch[2] . "\n";
+            switch ($hostMatch[2]) {
+
+                case (preg_match('/master.*/', $hostMatch[2]) ? true : false) :
+                        array_push($mastersArray, $hostMatch[1]);
+                        echo "master";
+                    break;
+
+                case (preg_match('/slave.*/', $hostMatch[2]) ? true : false) : 
+                    array_push($slavesArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/analytics.*/', $hostMatch[2]) ? true : false) :
+                    array_push($analyticsArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/aggregator.*/', $hostMatch[2]) ? true : false) :
+                    array_push($aggregatorArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/sensor.*/', $hostMatch[2]) ? true : false) :
+                    array_push($sensorsArray, $hostMatch[1]);
+                    break;
+            }
         }
-
-
     }
     fclose($hosts);
 } else {
     // error opening the file.
     echo "Can't open /etc/hosts file \n\n";
 } 
+
 
 // -------------------------------------------------->>>    YAML EXAMPLE
 /*
--- tests.php
+++ tests.php
@@ -7,10 +7,31 @@ if ($hosts) {
         // ((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])
 
         if (preg_match('/((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s+(.*[^\s])/', $line, $hostMatch)){ 
-            echo "\nHost -> "; echo $hostMatch[1] . " --> " . $hostMatch[0] . "\n";
+            echo "\nHost -> "; echo $hostMatch[1] . " --> " . $hostMatch[2] . "\n";
+            switch ($hostMatch[2]) {
+
+                case (preg_match('/master.*/', $hostMatch[2]) ? true : false) :
+                        array_push($mastersArray, $hostMatch[1]);
+                        echo "master";
+                    break;
+
+                case (preg_match('/slave.*/', $hostMatch[2]) ? true : false) : 
+                    array_push($slavesArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/analytics.*/', $hostMatch[2]) ? true : false) :
+                    array_push($analyticsArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/aggregator.*/', $hostMatch[2]) ? true : false) :
+                    array_push($aggregatorArray, $hostMatch[1]);
+                    break;
+
+                case (preg_match('/sensor.*/', $hostMatch[2]) ? true : false) :
+                    array_push($sensorsArray, $hostMatch[1]);
+                    break;
+            }
         }
-
-
     }
     fclose($hosts);
 } else {
