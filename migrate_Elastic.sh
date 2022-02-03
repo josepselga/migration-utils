4a3e9623177d3fe181a11261d5deba3ba74e52fd
Josep Selga <josep.selga@opencloudfactory.com>
2022-02-03 10:23:51 +0100

    aa

--- migrate_Elastic.sh
+++ migrate_Elastic.sh
@@ -2,3 +2,10 @@
 
 # OpenNAC 1.2.1 ElasticSearch migration to 1.2.2
 
+
+newAnalytics = $arg1
+
+
+elasticdump --input=http://localhost:9200/my_index --output=http://$newAnalytics:9200/my_index --type=mapping
+
+elasticdump --input=http://localhost:9200/my_index --output=http://$newAnalytics:9200/my_index --type=data
